234e3dd7637a0d2cc7d013ee17347f4ab5ec812b
LAZRAG Khalid <lazrag.khalid@uae.ac.ma>
2026-06-13 15:16:05 +0100

    feat: Include full name in sign-up data for improved user identification

--- app/src/main/java/com/mobile/shopease/data/repository/AuthRepository.kt
+++ app/src/main/java/com/mobile/shopease/data/repository/AuthRepository.kt
@@ -6,15 +6,19 @@ import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.Facebook
 import io.github.jan.supabase.gotrue.providers.Google
 import io.github.jan.supabase.gotrue.providers.builtin.Email
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 class AuthRepository {
     private val oauthRedirectUrl =
         "${BuildConfig.SUPABASE_AUTH_SCHEME}://${BuildConfig.SUPABASE_AUTH_HOST}"
 
     suspend fun signUp(email: String, pass: String) {
+        val fullName = email.substringBefore("@")
         SupabaseClient.client.auth.signUpWith(Email) {
             this.email = email
             this.password = pass
+            data = buildJsonObject { put("full_name", fullName) }
         }
     }
 
