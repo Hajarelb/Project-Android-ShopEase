b45d2f5b733113085e7f7526b33877a1be48c334
LAZRAG Khalid <lazrag.khalid@uae.ac.ma>
2026-06-13 15:15:38 +0100

    feat: Add Kotlin serialization plugin to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 val localProperties = Properties()
@@ -94,6 +95,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
 }
